151d0413143839de927c9a1294e48e0bbd7e0f47
Shaurya Dhadwal <shauryadhadwal@gmail.com>
2019-06-28 19:43:40 +0530

    Orders.js

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Component } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { connect } from 'react-redux';
 import Order from '../../components/Order/Order';
 import axios from '../../axios-orders';
@@ -6,15 +6,14 @@ import withErrorHandler from '../../hoc/WithErrorHandler/WithErrorHandler';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Orders.css';
 
-class Orders extends Component {
+const orders = (props) => {
 
-    state = {
-        loading: true,
-        orders: []
-    }
+    const [loading, setLoading] = useState(true); 
+    const [ordersList, setOrdersList] = useState([]); 
 
-    componentDidMount() {
-        const queryParams = `?auth=${this.props.token}&orderBy="userId"&equalTo="${this.props.userId}"`;
+    useEffect(() => {
+
+        const queryParams = `?auth=${props.token}&orderBy="userId"&equalTo="${props.userId}"`;
 
         axios.get('/orders.json' + queryParams)
             .then(res => {
@@ -22,33 +21,33 @@ class Orders extends Component {
                 for (let key in res.data) {
                     fetchedOrders.push({ ...res.data[key], id: key });
                 }
-                this.setState({ loading: false, orders: fetchedOrders });
+                setLoading(false);
+                setOrdersList(fetchedOrders);
             })
             .catch(err => {
-                this.setState({ loading: false });
+                setLoading(false);
             })
+    }, [])
+
+
+    let ordersRender =
+        ordersList.map(order => (
+            <Order key={order.id}
+                ingredients={order.ingredients}
+                price={order.price} address={order.orderData} />
+        ))
+
+    if (loading) {
+        ordersRender = <Spinner />
     }
 
-    render() {
-
-        let orders =
-            this.state.orders.map(order => (
-                <Order key={order.id}
-                    ingredients={order.ingredients}
-                    price={order.price} address={order.orderData} />
-            ))
-
-        if (this.state.loading) {
-            orders = <Spinner />
-        }
-        return (
-            <Fragment>
-                <div className={classes.OrderListContainer}>
-                    {orders}
-                </div>
-            </Fragment>
-        );
-    }
+    return (
+        <Fragment>
+            <div className={classes.OrderListContainer}>
+                {ordersRender}
+            </div>
+        </Fragment>
+    );
 }
 
 const mapStateToProps = state => {
@@ -63,4 +62,4 @@ const mapDispatchToProps = dispatch => {
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Orders, axios));
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(orders, axios));
