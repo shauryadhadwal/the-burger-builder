a5f6d391f1ebaa2be4220f2c304fde2c3bae1076
Shaurya Dhadwal <shauryadhadwal@gmail.com>
2019-06-28 19:41:44 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React, { Component } from 'react';
+import React, { useEffect, Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import * as actions from './store/actions/index';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Auth from './containers/Auth/Auth';
-import About from './containers/About/About';
 import Logout from './containers/Auth/Logout/Logout';
-import * as actions from './store/actions/index';
-import asyncComponent from './hoc/asyncComponent/asyncComponent';
-const asyncCheckout = asyncComponent(() => {
+import Auth from './containers/Auth/Auth'
+
+// Lazy Components
+const About = React.lazy(() => {
+    return import('./containers/About/About');
+});
+const Checkout = React.lazy(() => {
     return import('./containers/Checkout/Checkout');
 });
-const asyncOrders = asyncComponent(() => {
+const Orders = React.lazy(() => {
     return import('./containers/Orders/Orders');
 });
 
 const ProtectedRoute = (props) => {
-    const { path, component } = props;
+    const { path, component, render } = props;
     if (props.isAuth) {
+        // When lazy components are passed
+        if(render){
+            return <Route path={path} render={() => render} />
+        }
         return <Route path={path} component={component} />
     }
     else {
@@ -25,29 +32,33 @@ const ProtectedRoute = (props) => {
     }
 }
 
-class App extends Component {
+const app = (props) => {
 
-    componentDidMount() {
-        this.props.onTryAutoSignup();
-    }
+    useEffect(() => {
+        props.onTryAutoSignup();
+    }, [])
 
-    render() {
-        return (
-            <React.Fragment>
-                <Layout>
-                    <Switch>
-                        <Route path="/home" component={BurgerBuilder} />
-                        <Route path="/auth" component={Auth} />
-                        <Route path="/about" component={About} />
-                        <ProtectedRoute path="/orders" component={asyncOrders} isAuth={this.props.isAuth} />
-                        <ProtectedRoute path="/checkout" component={asyncCheckout} isAuth={this.props.isAuth} />
-                        <ProtectedRoute path="/logout" component={Logout} isAuth={this.props.isAuth} />
-                        <Redirect to="/home" />
-                    </Switch>
-                </Layout>
-            </React.Fragment>
-        );
-    }
+    const routes = (
+        <Switch>
+            <Route path="/home" component={BurgerBuilder} />
+            <Route path="/auth" component={Auth} />
+            <Route path="/about" render={() => <About />} />
+            <ProtectedRoute path="/orders" render={<Orders />} isAuth={props.isAuth} />
+            <ProtectedRoute path="/checkout" render={<Checkout />} isAuth={props.isAuth} />
+            <ProtectedRoute path="/logout" component={Logout} isAuth={props.isAuth} />
+            <Redirect to="/home" />
+        </Switch>
+    )
+
+    return (
+        <React.Fragment>
+            <Layout>
+                <Suspense fallback={<p>Loading...</p>}>
+                    {routes}
+                </Suspense>
+            </Layout>
+        </React.Fragment>
+    );
 }
 
 const mapStateToProps = state => {
@@ -62,4 +73,4 @@ const mapDispatchToProps = dispatch => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(app);
